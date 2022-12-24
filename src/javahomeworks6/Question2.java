bbb27bbd562cf6124db448d38f8a7632a6e4ea7f
Kenan Göztas <kenangoz0106@gmail.com>
2022-12-24 17:55:22 +0100

    ..

---
+++ src/javahomeworks6/Question2.java
@@ -0,0 +1,6 @@
+package javahomeworks6;
+
+public class Question2 {
+   // 2. Github hessabindan ders6 yi kendi local calisma ortaminiza pull ediniz. Araba
+    //classi icindeki ozellikleri kullanarak Constructor classinda nesne uretiniz.
+}
